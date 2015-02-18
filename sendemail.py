fec6f7ac1f5c9776939be4a5c70e9ea6f1b0d371
cruzemcfarlane <cruze.mcfarlane@hotmail.com>
2015-02-18 18:47:46 +0000

    customize my app

--- sendemail.py
+++ sendemail.py
@@ -1,30 +1,40 @@
 import smtplib
 
 fromaddr = "info3180.cruze@gmail.com"
-toaddr = "cruzemcfarlane@gmail.com"
+toaddr = "cruze.mcfarlane@hotmail.com"
 
 message = """From: {} <{}>
+
 To: {} <{}>
+
 Subject: {}
 
 {}
+
 """
 
+fromname = "Cruze Martinez"
+fromaddr = "info3180.cruze@gmail.com"
+toname = "Cruze McFarlane"
+toaddrs = "cruze.mcfarlane@hotmail.com"
+subject = "Yea it works!"
+msg = "Damn this is awesome"
+
 messagetosend = message.format(
-                               fromname,
-                               fromaddr,
-                               toname, 
-                               toaddr, 
-                               subject, 
-                               msg
-                               )
+ fromname,
+ fromaddr,
+ toname,
+ toaddr,
+ subject,
+ msg)
+
 #Credentials (if needed)
 username = 'info3180.cruze@gmail.com'
-password = 'Naruto12?'
+password = 'kxjfgocykwketvrw'
 
-#The actual mail send
+# The actual mail send
 server = smtplib.SMTP('smtp.gmail.com:587')
-server.startls()
-server.login(username, password)
-server.sendmail(fromaddr, toaddrs, messagetosend)
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddr, messagetosend)
 server.quit()
